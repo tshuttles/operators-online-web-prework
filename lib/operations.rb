3653aefca1a2c8ee4709717f56bd12d9cd9e9c84
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-06 03:07:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60 == 
+    "unsafe"
+  elsif speed < 40 == 
+    "unsafe"
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	40 > speed == 0 && speed < 60 == 0 ? "not safe" : false
 end
 	
 
