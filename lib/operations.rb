a7fd58372eb0122a8f2c3aa534120c40cbfab187
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-06 03:28:22 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
-
+def unsafe?(speed = 70 || 30)
+  if speed > 60 
+    true
+  elsif speed < 40 
+    true 
+  else
+    false
+  end
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?(speed = 70 || 30)
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
